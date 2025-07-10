87117ba2fa8a7de67a137a5a5a8a2eaf858da6e8
noy00y <ozairtheboss@gmail.com>
2025-07-10 00:05:13 -0400

    test for top lvl pipelining

--- hls_files/drop.cpp
+++ hls_files/drop.cpp
@@ -1,5 +1,6 @@
 #include "ap_int.h"
 #include "hls_stream.h"
+#include "ap_fixed.h"
 #include "hls_math.h"
 
 // -----------------------------------------------------------------------------
@@ -21,16 +22,31 @@
 #define W4  (W3/2)
 #define N2    32
 
-typedef float     data_t;
+typedef ap_fixed<16, 4> data_t;
 typedef unsigned char flag_t;
+
+// -----------------------------------------------------------------------------
+// Declarations for new split functions
+// -----------------------------------------------------------------------------
+extern "C" {
+void load_weights(
+    const data_t *weights1, const data_t *bias1,
+    const data_t *weights2, const data_t *bias2,
+    const data_t *FC1_W,    const data_t *FC1_B,
+    const data_t *FC2_W,    const data_t *FC2_B);
+void compute_cnn(
+    const data_t *img_in,
+    flag_t *flag_out);
+}
 
 // -----------------------------------------------------------------------------
 // Stage 0: Stream input frame into an HLS stream (pixel by pixel)
 // -----------------------------------------------------------------------------
 void stream_img(
-    const data_t *img_in,
-    hls::stream<data_t> &img_strm)
+    hls::stream<data_t> &img_strm,
+    const data_t *img_in)
 {
+#pragma HLS INLINE off
   for (int idx = 0; idx < H*W*C0; idx++) {
   #pragma HLS PIPELINE II=1
     img_strm.write(img_in[idx]);
@@ -42,30 +58,28 @@ void stream_img(
 // -----------------------------------------------------------------------------
 void conv1_stream(
     hls::stream<data_t> &img_strm,
-    const data_t *w1, const data_t *b1,
+    const data_t w1_local[M1*C0*K*K],
+    const data_t b1_local[M1],
     hls::stream<data_t> &feat1_strm)
 {
-  // line buffer for K-1 rows × W cols × C0 channels
+#pragma HLS INLINE off
   static data_t linebuf[K-1][W][C0];
   data_t window[K][K][C0];
 
   ROW_LOOP: for (int i = 0; i < H; i++) {
     COL_LOOP: for (int j = 0; j < W; j++) {
       PIX_CH: for (int c = 0; c < C0; c++) {
-      #pragma HLS PIPELINE II=1
-        // shift older lines up
-        for (int m = 0; m < K-2; m++) {
+      #pragma HLS PIPELINE II=4
+        for (int m = 0; m < K-2; m++)
           linebuf[m][j][c] = linebuf[m+1][j][c];
-        }
-        // read new pixel
+
         data_t px = img_strm.read();
         linebuf[K-2][j][c] = px;
 
-        // construct 3×3 window
         for (int m = 0; m < K; m++) {
           for (int n = 0; n < K; n++) {
             if (i + m < K-1 || j + n < K-1) {
-              window[m][n][c] = 0.0f;
+              window[m][n][c] = 0;
             } else {
               window[m][n][c] = (m < K-1)
                 ? linebuf[m][j + n - (K-1)]
@@ -75,20 +89,17 @@ void conv1_stream(
         }
       }
 
-      // once full window is available, emit M1 outputs
       if (i >= K-1 && j >= K-1) {
-        int oi = i - (K-1), oj = j - (K-1);
         for (int m = 0; m < M1; m++) {
-        #pragma HLS PIPELINE II=1
-          data_t acc = b1[m];
-          for (int p = 0; p < K; p++)
-          for (int q = 0; q < K; q++)
-          for (int c = 0; c < C0; c++) {
-            int widx = ((m*C0 + c)*K + p)*K + q;
-            acc += window[p][q][c] * w1[widx];
-          }
-          // ReLU
-          feat1_strm.write(acc > 0 ? acc : 0.0f);
+        #pragma HLS PIPELINE II=4
+          data_t acc = b1_local[m];
+          CONV1_MAC: for (int p = 0; p < K; p++)
+           for (int q = 0; q < K; q++)
+            for (int c = 0; c < C0; c++) {
+              int widx = ((m*C0 + c)*K + p)*K + q;
+              acc += window[p][q][c] * w1_local[widx];
+            }
+          feat1_strm.write(acc > 0 ? acc : (data_t)0);
         }
       }
     }
@@ -102,18 +113,18 @@ void pool1_stream(
     hls::stream<data_t> &feat1_strm,
     hls::stream<data_t> &feat1_p_strm)
 {
-  // simple 2×2 buffer toggling per row/col
+#pragma HLS INLINE off
   static data_t buf[2][2][M1];
   for (int r = 0; r < H1; r++) {
     for (int c = 0; c < W1; c++) {
       for (int m = 0; m < M1; m++) {
-      #pragma HLS PIPELINE II=1
+      #pragma HLS PIPELINE II=4
         data_t v = feat1_strm.read();
         buf[r%2][c%2][m] = v;
-        if ((r%2==1) && (c%2==1)) {
+        if ((r%2)==1 && (c%2)==1) {
           data_t m0 = buf[0][0][m], m1 = buf[0][1][m];
           data_t m2 = buf[1][0][m], m3 = buf[1][1][m];
-          data_t mx = (m0>m1?m0:m1), my = (m2>m3?m2:m3);
+          data_t mx = m0>m1?m0:m1, my = m2>m3?m2:m3;
           feat1_p_strm.write(mx>my?mx:my);
         }
       }
@@ -126,44 +137,44 @@ void pool1_stream(
 // -----------------------------------------------------------------------------
 void conv2_stream(
     hls::stream<data_t> &feat1_p_strm,
-    const data_t *w2, const data_t *b2,
+    const data_t w2_local[M2*M1*K*K],
+    const data_t b2_local[M2],
     hls::stream<data_t> &feat2_strm)
 {
+#pragma HLS INLINE off
   static data_t linebuf[K-1][W2][M1];
   data_t window[K][K][M1];
 
   ROW2: for (int i = 0; i < H2; i++) {
     COL2: for (int j = 0; j < W2; j++) {
       CH2: for (int c = 0; c < M1; c++) {
-      #pragma HLS PIPELINE II=1
-        // shift lines
-        for (int m = 0; m < K-2; m++) {
+      #pragma HLS PIPELINE II=4
+        for (int m = 0; m < K-2; m++)
           linebuf[m][j][c] = linebuf[m+1][j][c];
-        }
+
         data_t px = feat1_p_strm.read();
         linebuf[K-2][j][c] = px;
 
-        // build window
         for (int m = 0; m < K; m++)
-        for (int n = 0; n < K; n++)
-          window[m][n][c] = (i+m < K-1 || j+n < K-1)
-            ? 0.0f
-            : ((m < K-1)
-               ? linebuf[m][j + n - (K-1)]
-               : px);
+          for (int n = 0; n < K; n++)
+            window[m][n][c] = (i+m < K-1 || j+n < K-1)
+              ? (data_t)0
+              : ((m < K-1)
+                 ? linebuf[m][j + n - (K-1)]
+                 : px);
       }
 
       if (i >= K-1 && j >= K-1) {
         for (int m = 0; m < M2; m++) {
-        #pragma HLS PIPELINE II=1
-          data_t acc = b2[m];
-          for (int p = 0; p < K; p++)
-          for (int q = 0; q < K; q++)
-          for (int c = 0; c < M1; c++) {
-            int widx = ((m*M1 + c)*K + p)*K + q;
-            acc += window[p][q][c] * w2[widx];
-          }
-          feat2_strm.write(acc > 0 ? acc : 0.0f);
+        #pragma HLS PIPELINE II=4
+          data_t acc = b2_local[m];
+          CONV2_MAC: for (int p = 0; p < K; p++)
+           for (int q = 0; q < K; q++)
+             for (int c = 0; c < M1; c++) {
+               int widx = ((m*M1 + c)*K + p)*K + q;
+               acc += window[p][q][c] * w2_local[widx];
+             }
+          feat2_strm.write(acc > 0 ? acc : (data_t)0);
         }
       }
     }
@@ -177,17 +188,18 @@ void pool2_stream(
     hls::stream<data_t> &feat2_strm,
     hls::stream<data_t> &feat2_p_strm)
 {
+#pragma HLS INLINE off
   static data_t buf[2][2][M2];
   for (int r = 0; r < H3; r++) {
     for (int c = 0; c < W3; c++) {
       for (int m = 0; m < M2; m++) {
-      #pragma HLS PIPELINE II=1
+      #pragma HLS PIPELINE II=4
         data_t v = feat2_strm.read();
         buf[r%2][c%2][m] = v;
-        if ((r%2==1) && (c%2==1)) {
+        if ((r%2)==1 && (c%2)==1) {
           data_t m0 = buf[0][0][m], m1 = buf[0][1][m];
           data_t m2 = buf[1][0][m], m3 = buf[1][1][m];
-          data_t mx = (m0>m1?m0:m1), my = (m2>m3?m2:m3);
+          data_t mx = m0>m1?m0:m1, my = m2>m3?m2:m3;
           feat2_p_strm.write(mx>my?mx:my);
         }
       }
@@ -203,27 +215,28 @@ void flatten_stream(
     data_t vec1[M2*H4*W4],
     int &cnt)
 {
+#pragma HLS INLINE off
   cnt = 0;
   const int TOTAL = M2 * H4 * W4;
   for (int i = 0; i < TOTAL; i++) {
-  #pragma HLS PIPELINE II=1
+  #pragma HLS PIPELINE II=4
     vec1[cnt++] = feat2_p_strm.read();
   }
 }
 
 // -----------------------------------------------------------------------------
-// Stage 6: FC1 + Tanh (unchanged)
+// Stage 6: FC1 + Tanh
 // -----------------------------------------------------------------------------
 void fc1(
     const data_t vec1[M2*H4*W4], int cnt,
     const data_t *FC1_W, const data_t *FC1_B,
     data_t vec2[N2])
 {
+#pragma HLS INLINE off
   for (int o = 0; o < N2; o++) {
-  #pragma HLS PIPELINE II=1
+  #pragma HLS PIPELINE II=4
     data_t acc = FC1_B[o];
     for (int i = 0; i < cnt; i++) {
-    #pragma HLS UNROLL factor=2
       acc += vec1[i] * FC1_W[o * cnt + i];
     }
     vec2[o] = hls::tanh(acc);
@@ -238,13 +251,110 @@ void fc2(
     const data_t *FC2_W, const data_t *FC2_B,
     flag_t *flag_out)
 {
+#pragma HLS INLINE off
   data_t acc = FC2_B[0];
   for (int i = 0; i < N2; i++) {
-  #pragma HLS PIPELINE II=1
+  #pragma HLS PIPELINE II=4
     acc += vec2[i] * FC2_W[i];
   }
-  data_t p = 1.0f / (1.0f + hls::exp(-acc));
-  *flag_out = (p > 0.5f) ? 1 : 0;
+  data_t p = (data_t)1.0 / ((data_t)1.0 + hls::exp(-acc));
+  *flag_out = (p > (data_t)0.5) ? (flag_t)1 : (flag_t)0;
+}
+
+// -----------------------------------------------------------------------------
+// Weight loading into BRAM
+// -----------------------------------------------------------------------------
+void load_weights(
+    const data_t *weights1, const data_t *bias1,
+    const data_t *weights2, const data_t *bias2,
+    const data_t *FC1_W,    const data_t *FC1_B,
+    const data_t *FC2_W,    const data_t *FC2_B)
+{
+#pragma HLS INLINE off
+  // Local BRAM buffers
+  static data_t w1_local[M1*C0*K*K];
+  static data_t b1_local[M1];
+  static data_t w2_local[M2*M1*K*K];
+  static data_t b2_local[M2];
+  static data_t fc1_w_local[N2*(M2*H4*W4)];
+  static data_t fc1_b_local[N2];
+  static data_t fc2_w_local[N2];
+  static data_t fc2_b_local[1];
+
+  // Load weights1 and bias1
+  for (int i = 0; i < M1*C0*K*K; i++) {
+  #pragma HLS PIPELINE II=1
+    w1_local[i] = weights1[i];
+  }
+  for (int i = 0; i < M1; i++) {
+  #pragma HLS PIPELINE II=1
+    b1_local[i] = bias1[i];
+  }
+  // Load weights2 and bias2
+  for (int i = 0; i < M2*M1*K*K; i++) {
+  #pragma HLS PIPELINE II=1
+    w2_local[i] = weights2[i];
+  }
+  for (int i = 0; i < M2; i++) {
+  #pragma HLS PIPELINE II=1
+    b2_local[i] = bias2[i];
+  }
+  // Load FC1
+  int fc1_size = N2*(M2*H4*W4);
+  for (int i = 0; i < fc1_size; i++) {
+  #pragma HLS PIPELINE II=1
+    fc1_w_local[i] = FC1_W[i];
+  }
+  for (int i = 0; i < N2; i++) {
+  #pragma HLS PIPELINE II=1
+    fc1_b_local[i] = FC1_B[i];
+  }
+  // Load FC2
+  for (int i = 0; i < N2; i++) {
+  #pragma HLS PIPELINE II=1
+    fc2_w_local[i] = FC2_W[i];
+  }
+  #pragma HLS PIPELINE II=1
+  fc2_b_local[0] = FC2_B[0];
+}
+
+// -----------------------------------------------------------------------------
+// Compute function: full dataflow CNN
+// -----------------------------------------------------------------------------
+void compute_cnn(
+    const data_t *img_in,
+    flag_t *flag_out)
+{
+  // Streams for dataflow
+  hls::stream<data_t> img_strm("img_strm");
+  hls::stream<data_t> feat1_strm("feat1_strm");
+  hls::stream<data_t> feat1_p_strm("feat1_p_strm");
+  hls::stream<data_t> feat2_strm("feat2_strm");
+  hls::stream<data_t> feat2_p_strm("feat2_p_strm");
+
+  // Local copies of weights/biases
+  extern data_t w1_local[M1*C0*K*K];
+  extern data_t b1_local[M1];
+  extern data_t w2_local[M2*M1*K*K];
+  extern data_t b2_local[M2];
+  extern data_t fc1_w_local[N2*(M2*H4*W4)];
+  extern data_t fc1_b_local[N2];
+  extern data_t fc2_w_local[N2];
+  extern data_t fc2_b_local[1];
+
+  // Buffer for flatten
+  static data_t vec1_local[M2*H4*W4];
+  int cnt;
+
+#pragma HLS DATAFLOW
+  stream_img       (img_strm,      img_in);
+  conv1_stream     (img_strm,      w1_local,  b1_local,  feat1_strm);
+  pool1_stream     (feat1_strm,    feat1_p_strm);
+  conv2_stream     (feat1_p_strm,  w2_local,  b2_local,  feat2_strm);
+  pool2_stream     (feat2_strm,    feat2_p_strm);
+  flatten_stream   (feat2_p_strm,  vec1_local, cnt);
+  fc1              (vec1_local,    cnt,       fc1_w_local, fc1_b_local, vec1_local);
+  fc2              (vec1_local,    fc2_w_local, fc2_b_local, flag_out);
 }
 
 // -----------------------------------------------------------------------------
@@ -274,43 +384,15 @@ void cnn_accel(
 #pragma HLS INTERFACE s_axilite port=ctrl   bundle=control
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
-  // ---------------------------------------------------------------------------
-  // Weight & bias BRAM buffers (small)
-  // ---------------------------------------------------------------------------
-  static data_t w1_local[M1*C0*K*K];
-  static data_t b1_local[M1];
-  static data_t w2_local[M2*M1*K*K];
-  static data_t b2_local[M2];
-  static data_t fc1_w_local[N2*(M2*H4*W4)];
-  static data_t fc1_b_local[N2];
-  static data_t fc2_w_local[N2];
-  static data_t fc2_b_local[1];
-
-  // Prefetch / load weights into BRAM on ctrl==1
   if (ctrl == 1) {
-    // (same PL load loops as before, omitted for brevity)
-    return;
+    // Load weights into on-chip BRAM
+    load_weights(weights1, bias1,
+                 weights2, bias2,
+                 FC1_W,    FC1_B,
+                 FC2_W,    FC2_B);
+  } else {
+    // Run the CNN dataflow pipeline
+    compute_cnn(img_in, flag_out);
   }
-
-  // Streams for dataflow
-  hls::stream<data_t> img_strm("img_strm");
-  hls::stream<data_t> feat1_strm("feat1_strm");
-  hls::stream<data_t> feat1_p_strm("feat1_p_strm");
-  hls::stream<data_t> feat2_strm("feat2_strm");
-  hls::stream<data_t> feat2_p_strm("feat2_p_strm");
-
-  // Static vector for FC1 input
-  static data_t vec1_local[M2*H4*W4];
-  int cnt;
-
-#pragma HLS DATAFLOW
-  stream_img       (img_in,        img_strm);
-  conv1_stream     (img_strm,      w1_local,  b1_local,  feat1_strm);
-  pool1_stream     (feat1_strm,    feat1_p_strm);
-  conv2_stream     (feat1_p_strm,  w2_local,  b2_local,  feat2_strm);
-  pool2_stream     (feat2_strm,    feat2_p_strm);
-  flatten_stream   (feat2_p_strm,  vec1_local, cnt);
-  fc1              (vec1_local,    cnt,       fc1_w_local, fc1_b_local, vec1_local); // reuse vec1_local as vec2
-  fc2              (vec1_local,    fc2_w_local, fc2_b_local, flag_out);
 }
 } // extern "C"

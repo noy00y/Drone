6a444f1e801a5e57ff695ec475a9bcbe8519197e
Ozair Khan <ozairtheboss@gmail.com>
2025-09-13 10:06:29 -0400

    initial testbench

--- rtl/conv_engine/conv_engine.srcs/sim_1/new/tb_window.sv
+++ rtl/conv_engine/conv_engine.srcs/sim_1/new/tb_window.sv
@@ -3,13 +3,13 @@ module window_tb;
     // local test params
     localparam int K = 3;
     localparam int PIXEL_W = 8;
-    localparam int IMG_H = 5;
-    localparam int IMG_W = 6;
+    localparam int IMG_H = 10;
+    localparam int IMG_W = 10;
 
     // DUT I/O:
     logic                   clk = 0;
     logic                   rst_n = 0;
-    always #5 clk = ~clk // 100 mhz
+    always #5 clk = ~clk; // 100 mhz
 
     logic                   valid_in;
     logic [PIXEL_W-1:0]     pixel_in;
@@ -33,5 +33,50 @@ module window_tb;
         .data_out
     );
 
+    // Rst
+    initial begin
+        valid_in = 0;
+        pixel_in = '0;
+        repeat (3) @(posedge clk);
+        rst_n = 1;
+    end
+
+    // Test img:
+    logic [PIXEL_W-1:0] img [0:IMG_H-1][0:IMG_W-1];
+    initial begin
+        for (int r = 0; r < IMG_H; r++) begin
+            for (int c = 0; c < IMG_W; c++) begin 
+                img[r][c] = r*16 + c;
+            end
+        end
+    end
+
+    // Feed piels into DUT:
+    initial begin
+        @(posedge rst_n);
+
+        for (int r = 0; r < IMG_H; r++) begin
+            for (int c = 0; c < IMG_W; c++) begin
+                @(posedge clk);
+                valid_in <= 1'b1;
+                pixel_in <= img[r][c];
+            end
+        end
+
+        @(posedge clk);
+        pixel_in = '0;
+        valid_in = 1'b0; 
+
+        // Drain pipe:
+        repeat (20) @(posedge clk);
+        $finish;
+    end
+
+    // DUT output:
+    always_ff @(posedge clk) begin
+        if (valid_out) begin
+            $display("[%0t] window produced: 0x%0h", $time, data_out);
+        end
+    end
 
 endmodule

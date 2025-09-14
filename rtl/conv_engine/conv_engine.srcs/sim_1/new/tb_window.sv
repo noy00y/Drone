04adc4ba17ed00f056f24021ebe75d875e3df859
Ozair Khan <ozairtheboss@gmail.com>
2025-09-13 21:02:52 -0400

    window control pipe alignment

--- rtl/conv_engine/conv_engine.srcs/sim_1/new/tb_window.sv
+++ rtl/conv_engine/conv_engine.srcs/sim_1/new/tb_window.sv
@@ -3,8 +3,8 @@ module window_tb;
     // local test params
     localparam int K = 3;
     localparam int PIXEL_W = 8;
-    localparam int IMG_H = 10;
-    localparam int IMG_W = 10;
+    localparam int IMG_H = 5;
+    localparam int IMG_W = 5;
 
     // DUT I/O:
     logic                   clk = 0;
@@ -13,6 +13,7 @@ module window_tb;
 
     logic                   valid_in;
     logic [PIXEL_W-1:0]     pixel_in;
+    
     logic                   valid_out;
     logic                   busy;
     logic [K*K*PIXEL_W-1:0] data_out;
@@ -43,18 +44,20 @@ module window_tb;
 
     // Test img:
     logic [PIXEL_W-1:0] img [0:IMG_H-1][0:IMG_W-1];
+
     initial begin
+        static int unsigned idx = 0;
         for (int r = 0; r < IMG_H; r++) begin
-            for (int c = 0; c < IMG_W; c++) begin 
-                img[r][c] = r*16 + c;
+            for (int c = 0; c < IMG_W; c++) begin
+            img[r][c] = idx[PIXEL_W-1:0]; // auto-truncates to PIXEL_W bits
+            idx++;
             end
         end
     end
 
     // Feed piels into DUT:
     initial begin
-        @(posedge rst_n);
-
+        // @(posedge rst_n);
         for (int r = 0; r < IMG_H; r++) begin
             for (int c = 0; c < IMG_W; c++) begin
                 @(posedge clk);
@@ -75,7 +78,7 @@ module window_tb;
     // DUT output:
     always_ff @(posedge clk) begin
         if (valid_out) begin
-            $display("[%0t] window produced: 0x%0h", $time, data_out);
+            $display("[%0t] window produced: 0x%0d", $time, data_out);
         end
     end
 

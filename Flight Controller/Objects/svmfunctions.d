fa35d975a2b016c5e7376f356104916e5f05195e
noy00y <ozairtheboss@gmail.com>
2025-10-20 18:36:51 -0500

    init of keil

---
+++ Flight Controller/Objects/svmfunctions.d
@@ -0,0 +1,18 @@
+./objects/svmfunctions.o: \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\SVMFunctions.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_linear_init_f32.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Include\dsp\svm_functions.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Include\arm_math_types.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Include\arm_math_memory.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Include\dsp\none.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Include\dsp\utils.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Include\dsp\svm_defines.h \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_linear_predict_f32.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_polynomial_init_f32.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_polynomial_predict_f32.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_rbf_init_f32.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_rbf_predict_f32.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_sigmoid_init_f32.c \
+  C:\Users\ozair\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.16.2\Source\SVMFunctions\arm_svm_sigmoid_predict_f32.c
---
+++ Flight Controller/readme.md
@@ -0,0 +1,2 @@
+# Manually load packages:
+`C:\Users\ozair\AppData\Local\Keil_v5\UV4>PackUnzip.exe ..\..\..\..\Downloads\Keil.MDK-Middleware.8.1.0.pack`
